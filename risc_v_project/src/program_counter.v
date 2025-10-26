5d772c87263614e5e32671d7989cafa9b9b03704
Dylan-Bellamy <90278426+Dylan-Bellamy@users.noreply.github.com>
2025-10-26 11:37:20 -0400

    Added program counter module
    This commit introduces a Verilog module for the program counter, which increments the instruction address on each clock cycle and resets to zero when reset is asserted to high.

---
+++ risc_v_project/src/program_counter.v
@@ -0,0 +1,21 @@
+/*
+    Program Counter
+    - Register that holds the memory address for the next instruction
+    - Inputs and Outputs
+        - Clock: 0->1 changes the pc counter to the next instruction
+        - Reset: Resets program counter to zero
+        - PC : The address of the next instruction
+*/
+module program_counter(
+    input clk,
+    input reset,
+    output [31:0] pc
+);
+    always @(posedge clk) begin
+        if (reset) begin
+            pc <= 32'h0;
+        end else begin
+            pc <= pc + 4;
+        end
+    end
+endmodule 
